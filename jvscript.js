6dbe03e1fc7049abcec41aa7fc975643cd728479
Ian Humphrey <sslicked@gmail.com>
2026-06-24 20:17:35 +0800

    Add files via upload

--- jvscript.js
+++ jvscript.js
@@ -2,36 +2,65 @@ const display = document.getElementById("display");
 const inputGrades = document.getElementById("inputGrades");
 const buttonOfJudgement = document.getElementById("buttonOfJudgement");
 
-buttonOfJudgement.addEventListener("click", function() {
+buttonOfJudgement.addEventListener("click", function () {
 
     let grade = parseFloat(inputGrades.value);
 
-    if (grade >= 1.00 && grade <=1.25 || grade >= 94 && grade <=100) {
-        display.value = "Excellent";
+    if (isNaN(grade)) {
+        display.value = "Enter a valid number";
+        return;
     }
-    else if (grade > 1.25 && grade <=1.75 || grade >= 88 && grade <=93) {
-        display.value = "Very Good";
+
+//Numerical grades ni siya
+    if (grade >= 1.00 && grade <= 5.00) {
+
+        if (grade <= 1.25) {
+            display.value = "Excellent";
+        }
+        else if (grade <= 1.75) {
+            display.value = "Very Good";
+        }
+        else if (grade <= 2.25) {
+            display.value = "Good";
+        }
+        else if (grade <= 2.75) {
+            display.value = "Satisfactory";
+        }
+        else if (grade <= 3.00) {
+            display.value = "Passing";
+        }
+        else {
+            display.value = "💀💀 Failed";
+        }
+
     }
-    else if (grade > 1.75 && grade <=2.25 || grade >= 82 && grade <=87) {
-        display.value = "Good";
+
+//Percentage Grades ni siya
+    else if (grade >= 0 && grade <= 100) {
+
+        if (grade >= 94) {
+            display.value = "Excellent";
+        }
+        else if (grade >= 88) {
+            display.value = "Very Good";
+        }
+        else if (grade >= 82) {
+            display.value = "Good";
+        }
+        else if (grade >= 76) {
+            display.value = "Satisfactory";
+        }
+        else if (grade >= 75) {
+            display.value = "Passing";
+        }
+        else {
+            display.value = "💀💀 Failed";
+        }
+
     }
-    else if (grade > 2.25 && grade <=2.75 || grade >= 76 && grade <=81) {
-        display.value = "Satisfactory";
-    }
-    else if (grade > 2.75 && grade <=3.00 || grade === 75) {
-        display.value = "Passing";
-    }
-    else if ((grade > 3.00 && grade <= 5.00) || (grade >= 0 && grade < 75)) {
-        display.value = "💀💀";
-    }
+
     else {
         display.value = "Invalid Grade";
     }
 
 });
-
-
-
-
-
-
